d9661178c9dae940fa25c4640c77485ee28a5b6a
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-10-28 16:07:04 -0200

    products and orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :products
+
+  map.resources :orders
+
   map.resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
