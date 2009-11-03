4594cdce3e23a547de9d3c080e76ccdc3a2764eb
Caue Guerra <caueguerra@gmail.com>
2009-11-03 10:30:54 -0200

    removing migration unused

--- db/migrate/20091028182228_create_price_field.rb
+++
@@ -1,9 +0,0 @@
-class CreatePriceField < ActiveRecord::Migration
-  def self.up
-    add_column :trainings, :price, :float
-  end
-
-  def self.down
-    remove_column :trainings, :price
-  end
-end
